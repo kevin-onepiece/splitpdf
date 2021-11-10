ac673ad01c716a323ac5cea2666332d79c6774ba
fuqiang <fuqiang@doctortech.com.cn>
2021-11-10 22:08:53 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+# create the root window
+root = tk.Tk()
+root.title('Tkinter File Dialog')
+root.resizable(False, False)
+root.geometry('300x150')
+
+
+def select_files():
+    filetypes = (
+        ('text files', '*.pdf'),
+        ('All files', '*.*')
+    )
+
+    filenames = fd.askopenfilenames(
+        title='Open files',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected Files',
+        message=filenames
+    )
+
+    for file in filenames:
+        split_pdf(file)
+
+def split_pdf(file):
+    print(file)
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open Files',
+    command=select_files
+)
+
+open_button.pack(expand=True)
+
+root.mainloop()
