8fe3ab21eee0af119de59a003cdef2ac1f07625c
fuqiang <fuqiang@doctortech.com.cn>
2021-11-10 22:44:34 +0800

    feat: 操作页数

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 
+import pdfplumber
+
 # create the root window
 root = tk.Tk()
 root.title('Tkinter File Dialog')
@@ -29,8 +31,15 @@ def select_files():
     for file in filenames:
         split_pdf(file)
 
-def split_pdf(file):
-    print(file)
+def split_pdf(filename):
+    print(filename)
+    with pdfplumber.open(filename) as pdf:
+        # print(pdf.pages)
+        # todo: 对不同的页数进行操作
+        first_page = pdf.pages[0].page_number
+
+        print(first_page)
+        # print(first_page.chars[0])
 
 # open button
 open_button = ttk.Button(
